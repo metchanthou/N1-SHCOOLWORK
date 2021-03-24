a08871b6347f6f8af6b331701ada81f84823ffda
Student <student@PNCA32-18.pnc.lan>
2021-03-24 14:40:08 +0700

    already

--- S2- PRACTICE - DOM/EX1 - show-hide/js/app.js
+++ S2- PRACTICE - DOM/EX1 - show-hide/js/app.js
@@ -2,13 +2,19 @@
 
 let showImage = () => {
     // your code here
+    const btnShow =document.querySelector('.img-group');
+    btnShow.style.display ='block';
+    btnShow.style.display ='flex';
+
 }
 let hideImage = () => {
    // your code here
+   const btnHide =document.querySelector('.img-group');
+   btnHide.style.display ='none';
 }
 
-let btnShow = // your code here;
-let btnHide = // your code here;
+let btnShow = document.querySelector('#btn-show')// your code here;
+let btnHide = document.querySelector('#btn-hide')// your code here;
 
 btnHide.addEventListener('click', hideImage);
 btnShow.addEventListener('click', showImage);
