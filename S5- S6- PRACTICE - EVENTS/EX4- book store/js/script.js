77c0bdfa6c887f64f1ef936936fc9e09ae64348a
Student <student@PNCA32-18.pnc.lan>
2021-03-31 14:52:32 +0700

    already

--- S5- S6- PRACTICE - EVENTS/EX4- book store/js/script.js
+++ S5- S6- PRACTICE - EVENTS/EX4- book store/js/script.js
@@ -1,57 +1,59 @@
 // 
-const bookDelete= document.querySelectorAll('li');
-bookDelete.forEach( item =>{
-  item.addEventListener('click',()=>{
-    item.remove();
-  })
-});
 //  FUNCTIONS -------------------------------------------------------------
 function deleteBook(event) {
-	
+	event.preventDefault();
   // 1- Check the event if raised on the button delete 
-  
  //  2  if yes, get the parent and remove it from the  bookList
+ if (event.target.className==='delete'){
+  let li =event.target.parentElement;
+  li.remove();
+}
 }
 
 function addBook(event) {
   event.preventDefault();
 
   // 1- Get the book name from the input field
-  const get_bookNAME =document.getElementById('add-book-textfield');
-
+  const getBook=document.querySelector('#add-book-textfield');
+   
   // 2- Create a new spam bookName for the book name, class name = name
-  const bookName =document.createElement('span');
-  bookName.classList.add('name');
-  bookName.textContent =get_bookNAME;
+  const new_Name=document.createElement('span');
+  new_Name.className ='name';
+  new_Name.textContent =getBook.value;
 
   // 3- Create a new spam deleteBtn for the button delete, class name = delete
-  const deleteBtn =document.createElement('span');
-  deleteBtn.classList.add('delete');
+  const new_Delete=document.createElement('span');
+  new_Delete.className ='delete';
+  new_Delete.textContent ='delete';
 
   // 4- Create a new li
-  const new_Li =document.createElement('li');
-  const getUl =document.querySelector('#book-list ul');
+  const new_Li=document.createElement('li');
+  new_Li.appendChild(new_Name);
+  new_Li.appendChild(new_Delete);
 
   // 5- Add bookName and deleteBtn to li and li to the  bookList UL
-  new_Li.appendChild(bookName);
-  new_Li.appendChild(deleteBtn);
-  getUl.appendChild(new_Li);
-  document.addEventListener('keyup', event =>{
-    if (event.key ==='+ Addr'){
-      const name =document.querySelector('add-book-textfield');
-      // console.log(name.textContent.value)
-    }
-  })
-
+ const ul=document.querySelector('ul');
+ ul.appendChild(new_Li);
+ console.log(ul);
+   
 }
 
-function searchBook(event) {
+function searchBook() {
   // 1- Get the search text
-
-  // 2- Loop on all LI
- 
+  let text =searchBookInput.value.toLowerCase();
+  let  listBook =document.querySelectorAll('li');
+  // Loop 
+    for (let li of listBook){
+        let lisname =li.firstChild.textContent.toLowerCase();
+        if (lisname.indexOf(text)===-1){
+            li.style.display ='none';
+        }else{
+            li.style.display ='';
+        }
+    }
     // Update the style of the LI (visible or hidden)
-   
+    console.log(text);
+  
   }
 
 
